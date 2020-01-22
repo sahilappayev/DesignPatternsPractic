ead79dfc3426004de92688e542dd7f8ee96d6c53
sahilappay <sahil.appayev@mail.ru>
2020-01-22 14:59:12 +0400

    new commit

--- enums/MyEnum.java
+++ enums/MyEnum.java
@@ -10,30 +10,30 @@ package enums;
  * @author Admin
  */
 public enum MyEnum { // ENUM ashagidaki MyEnum classinin daha qisa shekilde yazilmish formasidir. Ikisi de eyni funksiyani yerine yetiri.
-    
-    TOPLA(), CHIX() , VUR(), BOL();
-    
+
+    TOPLA(), CHIX(), VUR(), BOL();
+
     private String text;
 
-    MyEnum(String text){
+    MyEnum(String text) {
         this.text = text;
     }
-    
-    public static class MyEnum2{// Bu class bir nov ENUMDUR.
-        
+
+    public static final class MyEnum2 {// Bu class bir nov ENUMDUR.
+
         public static final MyEnum2 TOPLA = new MyEnum2();
         public static final MyEnum2 CHIX = new MyEnum2();
         public static final MyEnum2 VUR = new MyEnum2();
         public static final MyEnum2 BOL = new MyEnum2();
-        
+
         private String text;
 
         public MyEnum2() {
         }
-        
-        MyEnum2(String text){
-        this.text = text;
-    }
-    
+
+        MyEnum2(String text) {
+            this.text = text;
+        }
+
     }
 }
